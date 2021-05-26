29e05a82c631e9809aa66359c004b8aa8d62269d
Viplav Nayyar <52647445+Viplav1996@users.noreply.github.com>
2021-05-25 22:21:30 -0400

    changed message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send("My pipeline is working???"));
+app.get('/', (req, res) => res.send("pipeline is working"));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
